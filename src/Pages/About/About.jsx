faa39a2e624ad5d39779b9cbc72efd0b357323b7
SotaSill <sotasill@gmail.com>
2024-12-08 15:44:58 +0200

    some changes

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,11 +1,14 @@
 import HeaderBar from "../HeaderBar/HeaderBar"
+import css from "./About.module.css"
 
 function About() {
   return (
     <div>
-      <HeaderBar/>
+      <HeaderBar />
+      <div className={css.wrapper}></div>
+      <h2>About</h2>
     </div>
-  )
+  );
 }
 
 export default About
